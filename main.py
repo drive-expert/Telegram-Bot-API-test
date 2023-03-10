38a160d8f9dc5066f362f0389a89c54427492b89
drive-expert <drive.expert35@gmail.com>
2023-03-10 12:09:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+from lxml import html
+import csv
+import json
+import  re
+
+#url = 'https://api.telegram.org/bot5613872402:AAEg4idiUxj7wUHckPdPxVkLQlHML8DmXn8/getupdates'
+#res = requests.get(url)
+#print(res.content)
+#url = 'https://api.telegram.org/bot5613872402:AAEg4idiUxj7wUHckPdPxVkLQlHML8DmXn8/sendMessage?chat_id=-1733307750&text=Test%20Message'
+#res = requests.get(url)
+#ddd = res.text
+#print(ddd)
